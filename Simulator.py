5e49ec02e1ab45bc50bbb7b00fd4ca6883eb8cc0
Yulin Liu <liuyulin101@berkeley.edu>
2017-11-05 19:33:46 -0800

    add all

---
+++ Simulator.py
@@ -0,0 +1,97 @@
+import numpy as np
+from scipy import stats
+import os
+
+class Simulator:
+    def __init__(self, 
+                 seed = 101,
+                 N_prod = 2,
+                 Tstamp = 20,
+                 price = np.array([[1.5, 1.5]]),
+                 costQ = np.array([[0.1, 0.1]]),
+                 costInv = np.array([[0.2, 0.2]]),
+                 costLastInv = np.array([[1, 1]]),
+                 costBack = np.array([[0.5, 0.5]]) ):
+        self.seed = seed
+        self.N_prod = N_prod
+        self.Tstamp = Tstamp
+
+        self.price = price
+        self.costQ = costQ
+        self.costInv = costInv
+        self.costLastInv = costLastInv
+        self.costBack = costBack
+
+    def obs_dim(self):
+        return self.N_prod
+
+    def act_dim(self):
+        # add another axis in the future
+        return self.N_prod
+
+    def demandGenerator(self, mu, cov, seed = 123):
+        # generate multivariate log normal random variables
+        # mu should have shape of N_prod * 1
+        # cov should have shape of N_prod * N_prod
+        np.random.seed(seed)
+        self.demandVec = np.exp(np.random.multivariate_normal(mean = np.log(mu), cov = cov, size = self.Tstamp + 1))
+        return self.demandVec
+
+    def actionSpace(self):
+        # can be changed
+        return 0, 10.0
+
+    def randomActionGenerator(self, Nsample = 1):
+        minAction, maxAction = self.actionSpace()
+        return np.random.uniform(low = minAction, high = maxAction, size = (Nsample, self.N_prod))
+
+    def stateSpace(self):
+        return -10., 10.
+
+    def randomStateGenerator(self, Nsample = 1):
+        minInv, maxInv = self.stateSpace()
+        return np.random.uniform(low = minInv, high = maxInv, size = (Nsample, self.N_prod))
+
+    def randomInitialStateGenerator(self):
+        _, maxInv = self.stateSpace()
+        return np.random.uniform(low = 0, high = maxInv, size = (1, self.N_prod))
+
+
+    def _reward(self, action, inventory, demand, last = False):
+        # here action is a vector of q (1 by N) and is the action for timestamp t
+        # inventory has shape 1 by N and is the inventory for timestamp (t+1)
+        # demand has shape 1 by N and is the inventory for timestamp (t+1)
+        if last:
+            return (self.price.dot(demand.T) - (self.costQ.dot(action.T) + 
+                                 self.costLastInv.dot(np.maximum(np.zeros(shape = inventory.shape), inventory).T) - 
+                                 self.costBack.dot(np.minimum(np.zeros(shape = inventory.shape), inventory).T)))[0][0]
+        else:
+            return (self.price.dot(demand.T) - (self.costQ.dot(action.T) + 
+                                 self.costInv.dot(np.maximum(np.zeros(shape = inventory.shape), inventory).T) - 
+                                 self.costBack.dot(np.minimum(np.zeros(shape = inventory.shape), inventory).T)))[0][0]
+
+    def nextInventory(self, curInventory, curAction, curDemand):
+        return curInventory + curAction - curDemand
+
+    def step(self, action, currentInventory, curDemand, nextDemand, last):
+        nextInventory = self.nextInventory(currentInventory, action, curDemand)
+        reward = self._reward(action, nextInventory, nextDemand,last)
+        return nextInventory, reward
+
+
+
+# test
+# env = Simulator(seed = 1)
+# demand = env.demandGenerator(mu = np.array([5,5]), cov = np.diag(np.array([0.1, 0.1])))
+# randomAction = env.randomActionGenerator()
+# initState = env.randomStateGenerator()
+
+# rew = env._reward(env.randomActionGenerator(), env.randomStateGenerator(), demand[0,:], False)
+# rew2 = env._reward(env.randomActionGenerator(), env.randomStateGenerator(), demand[0,:], True)
+
+# print(rew)
+# print(rew2)
+# print(randomAction)
+# print(initState)
+# print(demand)
+# print(demand[0,:])
---
+++ train_pg.py
@@ -0,0 +1,302 @@
+import numpy as np
+import tensorflow as tf
+import gym
+import logz
+import os
+import time
+import inspect
+from Simulator import Simulator
+import random
+#============================================================================================#
+# Utilities
+#============================================================================================#
+
+def build_mlp(
+        input_placeholder, 
+        output_size,
+        scope, 
+        n_layers=2, 
+        size=64, 
+        activation=tf.tanh,
+        output_activation=None
+        ):
+    Layer = {}
+    with tf.variable_scope(scope):
+        for i in range(n_layers):
+            if i == 0:
+                Layer[i] = tf.layers.dense(inputs = input_placeholder, 
+                                        units = size,
+                                        activation = activation,
+                                        use_bias = True)
+            else:
+                Layer[i] = tf.layers.dense(inputs = Layer[i-1], 
+                                        units = size,
+                                        activation = activation,
+                                        use_bias = True)
+            
+        Layer['logits'] = tf.layers.dense(inputs = Layer[n_layers-1], 
+                                units = output_size,
+                                activation = output_activation,
+                                use_bias = True)
+    return Layer['logits']
+
+def pathlength(path):
+    return len(path["reward"])
+
+#============================================================================================#
+# Policy Gradient
+#============================================================================================#
+
+def train_PG(exp_name='',
+             n_iter=100, 
+             gamma=1.0, 
+             min_timesteps_per_batch=1000, 
+             learning_rate=5e-3, 
+             reward_to_go=True, 
+             logdir=None, 
+             normalize_advantages=True,
+             nn_baseline=True, 
+             seed=101,
+             # network arguments
+             n_layers=1,
+             size=32
+             ):
+    #========================================================================================#
+    # Logfile setup
+    #========================================================================================#
+    start = time.time()
+    # Configure output directory for logging
+    logz.configure_output_dir(logdir)
+
+    # Log experimental parameters
+    args = inspect.getargspec(train_PG)[0]
+    locals_ = locals()
+    params = {k: locals_[k] if k in locals_ else None for k in args}
+    logz.save_params(params)
+    # Set random seeds
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+    #========================================================================================#
+    # Env setup
+    #========================================================================================#
+    env = Simulator(seed = 101,
+                 N_prod = 2,
+                 Tstamp = 20,
+                 price = np.array([[1.5, 1.5]]),
+                 costQ = np.array([[0.1, 0.1]]),
+                 costInv = np.array([[0.2, 0.2]]),
+                 costLastInv = np.array([[1, 1]]),
+                 costBack = np.array([[0.5, 0.5]]) )
+    # Observation and action sizes
+    ob_dim = env.obs_dim()
+    ac_dim = env.act_dim()
+    print('observation dimension is: ', ob_dim)
+    print('action dimension is: ', ac_dim)
+    #========================================================================================#
+    # PG Network placeholders
+    #========================================================================================#
+    sy_ob_no = tf.placeholder(shape=[None, ob_dim], name="ob", dtype=tf.float32)
+    sy_ac_na = tf.placeholder(shape=[None, ac_dim], name="ac", dtype=tf.float32) 
+    sy_adv_n = tf.placeholder(shape = [None], name = 'adv', dtype = tf.float32)
+
+    sy_mean = build_mlp(input_placeholder = sy_ob_no, 
+                        output_size = ac_dim,
+                        scope = 'continuous' + str(seed), 
+                        n_layers=n_layers, 
+                        size=size)
+
+    sy_logstd = tf.Variable(tf.truncated_normal(shape = [1, ac_dim], stddev = 0.1))
+    sy_sampled_ac = sy_mean + tf.multiply(tf.random_normal(shape = tf.shape(sy_mean)), tf.exp(sy_logstd))
+    MVN_dist = tf.contrib.distributions.MultivariateNormalDiag(sy_mean, tf.exp(sy_logstd))
+    sy_logprob_n = MVN_dist.log_prob(sy_ac_na)
+
+    # Loss function for PG network
+    loss = -tf.reduce_mean(tf.multiply(sy_logprob_n, sy_adv_n)) # Loss function that we'll differentiate to get the policy gradient.
+    update_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    #========================================================================================#
+    # Critic network
+    #========================================================================================#
+    if nn_baseline:
+        baseline_prediction = tf.squeeze(build_mlp(
+                                sy_ob_no, 
+                                output_size = 1, 
+                                scope = "nn_baseline" + str(seed),
+                                n_layers = n_layers,
+                                size = size))
+        # Define placeholders for targets, a loss function and an update op for fitting a 
+        # neural network baseline. These will be used to fit the neural network baseline. 
+        baseline_target = tf.placeholder(shape = [None], name = 'baseline_target_qn', dtype = tf.float32)
+        baseline_loss = tf.nn.l2_loss(baseline_target - baseline_prediction)
+        baseline_update_op = tf.train.AdamOptimizer(learning_rate).minimize(baseline_loss)
+
+
+    #========================================================================================#
+    # Tensorflow Engineering: Config, Session, Variable initialization
+    #========================================================================================#
+    num_gpu = 0
+    tf_config = tf.ConfigProto(inter_op_parallelism_threads=1, intra_op_parallelism_threads=1, device_count = {'GPU': num_gpu}) 
+    sess = tf.Session(config=tf_config)
+    sess.__enter__() # equivalent to `with sess:`
+    tf.global_variables_initializer().run() #pylint: disable=E1101
+
+    #========================================================================================#
+    # Training Loop
+    #========================================================================================#
+
+    total_timesteps = 0
+    total_numpaths = 0
+    
+    for itr in range(n_iter):
+        randk = 0 + itr * seed
+        print("********** Iteration %i ************"%itr)
+
+        # Collect paths until we have enough timesteps
+        timesteps_this_batch = 0
+        num_path = 0
+        paths = []
+        while True:
+            demand = env.demandGenerator(mu = np.array([5, 5]), cov = np.diag(np.array([0.1, 0.1])), seed = randk)
+            # Could be optimized by generating a batch demand vector. Fix this later!
+            randk += 1
+            ob = env.randomInitialStateGenerator()
+            obs, acs, rewards = [], [], []
+            last = False
+            steps = 0
+
+            while steps < env.Tstamp:
+                if steps == env.Tstamp - 1:
+                    last = True
+                obs.append(ob.flatten())
+                ac = sess.run(sy_sampled_ac, feed_dict={sy_ob_no : ob})
+                # ac = ac[0]
+                acs.append(ac.flatten())
+                ob, rew = env.step(ac, ob, demand[steps, :], demand[steps+1, :], last)
+                rewards.append(rew)
+                steps += 1
+
+            path = {"observation" : np.array(obs), 
+                    "reward" : np.array(rewards), 
+                    "action" : np.array(acs)}
+            paths.append(path)
+            num_path += 1
+            timesteps_this_batch += pathlength(path)
+            if timesteps_this_batch > min_timesteps_per_batch:
+                break
+        total_numpaths += num_path
+        total_timesteps += timesteps_this_batch
+
+        # Build arrays for observation, action for the policy gradient update by concatenating 
+        # across paths
+        ob_no = np.concatenate([path["observation"] for path in paths])
+        ac_na = np.concatenate([path["action"] for path in paths])
+        # print(ob_no.shape)
+        # print(ac_na.shape)
+        # print(path['reward'].shape)
+
+        #========================================================================================#
+        # Compute Q value
+        #========================================================================================#
+        if reward_to_go:
+            q_n = np.concatenate([[np.npv((1/gamma - 1), path["reward"][i:]) for i in range(len(path["reward"]))] for path in paths])
+        else:
+            q_n = np.concatenate([[np.npv((1/gamma - 1), path["reward"])]*len(path["reward"]) for path in paths])        
+
+        #====================================================================================#
+        # Compute Baselines
+        #====================================================================================#
+
+        if nn_baseline:
+            q_n_mean = q_n.mean()
+            q_n_std = q_n.std()
+            q_n = (q_n - q_n_mean)/q_n_std
+            b_n = baseline_prediction
+            adv_n_baseline = q_n - b_n
+        else:
+            adv_n = q_n.copy()    
+
+        #====================================================================================#
+        # Optimizing Neural Network Baseline
+        #====================================================================================#
+        if nn_baseline:
+            _, adv_n = sess.run([baseline_update_op, adv_n_baseline], feed_dict={baseline_target: q_n, sy_ob_no: ob_no})
+            adv_n = adv_n * q_n_std + q_n_mean
+
+        #====================================================================================#
+        # Advantage Normalization
+        #====================================================================================#
+
+        if normalize_advantages:
+            adv_n = (adv_n - adv_n.mean())/adv_n.std()
+
+        #====================================================================================#
+        # Performing the Policy Update
+        #====================================================================================#
+        _, train_loss= sess.run([update_op, loss], feed_dict={sy_adv_n: adv_n, sy_ac_na: ac_na, sy_ob_no: ob_no})
+        print("PG Network training loss: %.5f"%train_loss)
+
+        # Log diagnostics
+        returns = [path["reward"].sum() for path in paths]
+        ep_lengths = [pathlength(path) for path in paths]
+        logz.log_tabular("Time", time.time() - start)
+        logz.log_tabular("Iteration", itr)
+        logz.log_tabular("AverageReturn", np.mean(returns))
+        logz.log_tabular("StdReturn", np.std(returns))
+        logz.log_tabular("MaxReturn", np.max(returns))
+        logz.log_tabular("MinReturn", np.min(returns))
+        logz.log_tabular("EpLenMean", np.mean(ep_lengths))
+        logz.log_tabular("EpLenStd", np.std(ep_lengths))
+        logz.log_tabular("NumPathsThisBatch", num_path)
+        logz.log_tabular("NumPathsSoFar", total_numpaths)
+        logz.log_tabular("TimestepsThisBatch", timesteps_this_batch)
+        logz.log_tabular("TimestepsSoFar", total_timesteps)
+        logz.dump_tabular()
+        logz.pickle_tf_vars()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', type=str, default='SingleAgent')
+    parser.add_argument('--discount', type=float, default=0.9)
+    parser.add_argument('--n_iter', '-n', type=int, default=500)
+    parser.add_argument('--batch_size', '-b', type=int, default=1280)
+    parser.add_argument('--learning_rate', '-lr', type=float, default=5e-3)
+    parser.add_argument('--reward_to_go', '-rtg', action='store_true')
+    parser.add_argument('--dont_normalize_advantages', '-dna', action='store_true')
+    parser.add_argument('--nn_baseline', '-bl', action='store_true')
+    parser.add_argument('--seed', type=int, default=666)
+    parser.add_argument('--n_experiments', '-e', type=int, default=1)
+    parser.add_argument('--n_layers', '-l', type=int, default=1)
+    parser.add_argument('--size', '-s', type=int, default=32)
+    args = parser.parse_args()
+
+    if not(os.path.exists('data')):
+        os.makedirs('data')
+    logdir = args.exp_name + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = os.path.join('data', logdir)
+    if not(os.path.exists(logdir)):
+        os.makedirs(logdir)
+
+    for e in range(args.n_experiments):
+        seed = args.seed + 10*e
+        print('Running experiment with seed %d'%seed)
+        train_PG(
+            exp_name=args.exp_name,   
+            n_iter=args.n_iter,
+            gamma=args.discount,
+            min_timesteps_per_batch=args.batch_size,
+            learning_rate=args.learning_rate,
+            reward_to_go=args.reward_to_go,
+            logdir=os.path.join(logdir,'%d'%seed),
+            normalize_advantages=not(args.dont_normalize_advantages),
+            nn_baseline=args.nn_baseline, 
+            seed=seed,
+            n_layers=args.n_layers,
+            size=args.size
+            )
+        
+
+if __name__ == "__main__":
+    main()
